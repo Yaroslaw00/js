631edfae6e4bad2684a400e7837025d256dc31d3
Yaroslaw00 <yarikost901@gmail.com>
2025-11-20 20:36:02 +0100

    ok

---
+++ index.js
@@ -0,0 +1,29 @@
+// const x = Number(prompt("Скільки в тебе грошей?"));
+// const y = Number(prompt("Скільки коштує товар?"));
+// alert("Ваша решта " + (x - y));
+
+const age = 18;
+console.log(age);
+
+const UserName = "Yaroslav";
+console.log(UserName);
+
+const iStudent = true;
+console.log(iStudent);
+
+const myNumber = 57;
+console.log(myNumber + 10);
+
+const isNull = null;
+console.log(isNull);
+
+const isName = prompt("Введіть своє ім’я");
+alert("Ваше ім'я: " + isName);
+alert("Ваш тип: " + typeof isName);
+
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log("Результат:", userAnswer);
+
+alert("Увага! Дія може бути небезпечною");
+const x = confirm("Ви впевнені, що хочете продовжити?");
+console.log(x);
