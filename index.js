9a1d727cb380d4a3058873d0babd37f429dc43a4
Yaroslaw00 <yarikost901@gmail.com>
2025-12-02 19:10:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,28 +2,39 @@
 // const y = Number(prompt("Скільки коштує товар?"));
 // alert("Ваша решта " + (x - y));
 
-const age = 18;
-console.log(age);
+// const age = 18;
+// console.log(age);
 
-const UserName = "Yaroslav";
-console.log(UserName);
+// const UserName = "Yaroslav";
+// console.log(UserName);
 
-const iStudent = true;
-console.log(iStudent);
+// const iStudent = true;
+// console.log(iStudent);
 
-const myNumber = 57;
-console.log(myNumber + 10);
+// const myNumber = 57;
+// console.log(myNumber + 10);
 
-const isNull = null;
-console.log(isNull);
+// const isNull = null;
+// console.log(isNull);
 
-const isName = prompt("Введіть своє ім’я");
-alert("Ваше ім'я: " + isName);
-alert("Ваш тип: " + typeof isName);
+// const isName = prompt("Введіть своє ім’я");
+// alert("Ваше ім'я: " + isName);
+// alert("Ваш тип: " + typeof isName);
 
-const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
-console.log("Результат:", userAnswer);
+// const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+// console.log("Результат:", userAnswer);
 
-alert("Увага! Дія може бути небезпечною");
-const x = confirm("Ви впевнені, що хочете продовжити?");
-console.log(x);
+// alert("Увага! Дія може бути небезпечною");
+// const x = confirm("Ви впевнені, що хочете продовжити?");
+// console.log(x);
+
+console.log(Math.round(10.5));
+
+console.log(Math.ceil(10.5)); // стеля 11 - округлює до більшого
+
+console.log(Math.floor(10.5)); // підлога - округлює до меншого
+
+console.log(parseInt(Math.random() * 100 + 1)); // від 0 до 1
+
+console.log(Math.max(20, 30, 25, 15));
+console.log(Math.min(20, 30, 25, 15));
