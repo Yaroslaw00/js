4d03ff3b02627ade2f0e18e342de732f52816bab
Yaroslaw00 <yarikost901@gmail.com>
2025-12-02 19:30:37 +0100

    ok

---
+++ hw-2.js
@@ -0,0 +1,8 @@
+// Створити змінні const integer = 42 та const stringNumber = "256" для зберігання цілочисельного значення та рядка з числом у вигляді рядка. Використати метод parseInt() для перетворення рядка у ціле число. Результат зберегти в змінній convertedInt та вивести результат в консоль. Потім використати метод toString() для перетворення цілочисельного значення у рядок. Результат зберегти в змінній convertedString та вивести результат в консоль.
+
+const integer = 42;
+const stringNumber = "256";
+const Int = parseInt(stringNumber);
+console.log(Int);
+const convertedInt = integer.toString();
+console.log(convertedInt);
