7272ce050f331505c8aa4d217fdc3b00abba9013
Yaroslaw00 <yarikost901@gmail.com>
2025-12-04 20:10:31 +0100

    ok

--- hw-2.js
+++ hw-2.js
@@ -1,10 +1,51 @@
-// Створити змінні const integer = 42 та const stringNumber = "256" для зберігання цілочисельного значення та рядка з числом у вигляді рядка. Використати метод parseInt() для перетворення рядка у ціле число. Результат зберегти в змінній convertedInt та вивести результат в консоль. Потім використати метод toString() для перетворення цілочисельного значення у рядок. Результат зберегти в змінній convertedString та вивести результат в консоль.
+// 1
+const celsius = 35;
+const fahrenheit = (celsius * 9) / 5 + 32;
+console.log(fahrenheit);
 
-// const integer = 42;
-// const stringNumber = "256";
-// const Int = parseInt(stringNumber);
-// console.log(Int);
-// const convertedInt = integer.toString();
-// console.log(convertedInt);
+// 2
+const daysInMonth = 30;
+const hoursInMonth = daysInMonth * 24;
+console.log(hoursInMonth);
+const minutesInMonth = hoursInMonth * 60;
+console.log(minutesInMonth);
 
+// 3
+// let health = prompt("Введіть кількість здоровья");
+// let damage = prompt("Введіть кількість урону");
+// health = health - damage;
+// console.log(health);
 
+// 4
+let totalPrice = 350;
+const discount = 10;
+const discountPrice = (totalPrice * 10) / 100;
+console.log(discountPrice);
+totalPrice = totalPrice - discountPrice;
+console.log(totalPrice);
+
+// 5
+const floatNumber = 12.78;
+const roundedDown = console.log(Math.floor(floatNumber));
+
+// 6
+const floatString = "45.67";
+const parsedfloat = console.log(parseFloat(floatString));
+
+// 7
+const intString = "123";
+const parsedInt = console.log(parseInt(intString));
+
+// 8
+
+const number = 67;
+const sqrtNumber = console.log(Math.sqrt(number));
+
+// 9
+
+const integer = 42;
+const stringNumber = "256";
+const Int = parseInt(stringNumber);
+console.log(Int);
+const convertedInt = integer.toString();
+console.log(convertedInt);
---
+++ index0f.js
@@ -0,0 +1,3 @@
+// const email = "user123@gmail.com";
+// const index = email.indexOf("g");
+// console.log(email.slice(index, index + 5));
