e37c56f6878f66f7975729376c0b650b6707c59a
Yaroslaw00 <yarikost901@gmail.com>
2025-12-11 18:28:30 +0100

    ок

---
+++ hw-3.js
@@ -0,0 +1,20 @@
+// 1
+// const result = 5 + 5 + "5";
+// console.log(result);
+// console.log(typeof result);
+
+// 2
+// const email = "player228@gmail.com";
+// const include = console.log(email.includes("g"));
+// const length1 = console.log(email.length);
+
+// 3
+const x = "My";
+const Name = "name";
+const y = "is";
+const fullName = console.log(`${x} ${Name} ${y} Yaroslav`);
+
+// 4
+const userName = prompt("Введіть ім'я");
+const payment = prompt("Введіть рахунок");
+alert("Дякуємо, " + userName + "!" + " До сплати " + payment + " гривень");
--- index0f.js
+++ index0f.js
@@ -1,3 +1,3 @@
-// const email = "user123@gmail.com";
+// const email = "player228@gmail.com";
 // const index = email.indexOf("g");
 // console.log(email.slice(index, index + 5));
