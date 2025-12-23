30b9371489663ffa399c73fc6b9b7cec23d6990b
Yaroslaw00 <yarikost901@gmail.com>
2025-12-23 22:28:19 +0100

    if else

--- lesson-4.js
+++ lesson-4.js
@@ -6,28 +6,51 @@
 // }
 
 // 1
-//  const x = "";
-// const y = "";
-// if (x !== "" && y !== "") {
-//   console.log("Обидва поля заповнені");
-// } else {
-//   console.log("Не всі поля заповнені");
-// }
+const x = "";
+const y = "";
+if (x !== "" && y !== "") {
+  console.log("Обидва поля заповнені");
+} else {
+  console.log("Не всі поля заповнені");
+}
 
 // 2
-// const number1 = 10;
-// const number2 = 5;
-// const summe = number1 + number2;
-// if (summe >= 10) {
-//   console.log("Сума більша за 10");
-// } else {
-//   console.log("Сума менша або дорівнює 10");
-// }
+const number1 = 10;
+const number2 = 5;
+const summe = number1 + number2;
+if (summe >= 10) {
+  console.log("Сума більша за 10");
+} else {
+  console.log("Сума менша або дорівнює 10");
+}
 
 // 3
-// const zminna = " Я використовую JavaScript";
-// if (zminna.includes("JavaScript")) {
-//   console.log("Текст містить слово JavaScript");
-// } else {
-//   console.log("Текст не містить слово JavaScript");
-// }
+const zminna = " Я використовую JavaScript";
+if (zminna.includes("JavaScript")) {
+  console.log("Текст містить слово JavaScript");
+} else {
+  console.log("Текст не містить слово JavaScript");
+}
+
+// 4
+const counter = prompt("Введіть число від 10 до 20");
+if (counter >= 10 && counter <= 20) {
+  console.log("Число входить в діапазон від 10 до 20");
+} else {
+  console.log("Число не входить в діапазон від 10 до 20");
+}
+
+//5
+const Name = "Yaroslav";
+const mail = "dmytro1337@gmail.com";
+const passwort = "dimon1234";
+if (
+  Name.length >= 3 &&
+  mail.includes("@") &&
+  mail.includes(".") &&
+  passwort.length >= 6
+) {
+  console.log("Перенаправлення на іншу сторінку");
+} else {
+  console.log("Помилка: неправильне заповнення");
+}
