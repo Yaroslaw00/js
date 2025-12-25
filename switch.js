cb5f284738f831249257327c57149066ff5d65f9
Yaroslaw00 <yarikost901@gmail.com>
2025-12-25 19:59:29 +0100

    switch

--- switch.js
+++ switch.js
@@ -33,3 +33,82 @@ switch (day) {
     console.log("Такого дня не існує");
     break;
 }
+
+// 3.Створіть змінну для зберігання номера місяця. За номером місяця визначайте пору року і виводьте відповідне повідомлення.
+
+const month = 0;
+switch (month) {
+  case 12:
+  case 1:
+  case 2:
+    console.log("Пора року Зима");
+    break;
+  case 3:
+  case 4:
+  case 5:
+    console.log("Пора року Весна");
+    break;
+  case 6:
+  case 7:
+  case 8:
+    console.log("Пора року Літо");
+    break;
+  case 9:
+  case 10:
+  case 11:
+    console.log("Пора року Осінь");
+    break;
+
+  default:
+    console.log("Такої пори року нема, братиш");
+    break;
+}
+
+// 4.Створіть змінну для зберігання назви кольору. Виводьте повідомлення відповідно до вибраного кольору: якщо "червоний" — "стоп", "зелений" — "йти", "жовтий" — "чекати".
+
+const color = "green";
+switch (color) {
+  case "green":
+    console.log("Йти");
+    break;
+  case "red":
+    console.log("Стоп");
+    break;
+  case "yellow":
+    console.log("Чекати");
+    break;
+
+  default:
+    console.log("Такого кольору нема, братиш");
+    break;
+}
+
+// 5.Створіть змінні для зберігання двох чисел та оператора (як у списку select). Виконайте відповідну операцію та виведіть результат. У випадку ділення на нуль — виведіть попереджен
+let a = 10;
+let b = 0;
+let operator = "/"; // "+", "-", "*", "/"
+
+let result;
+
+switch (operator) {
+  case "+":
+    result = a + b;
+    break;
+
+  case "-":
+    result = a - b;
+    break;
+
+  case "*":
+    result = a * b;
+    break;
+
+  case "/":
+    result = a / b;
+    console.warn("Помилка: ділення на нуль!");
+
+    break;
+
+  default:
+    console.warn("Невідомий оператор");
+}
