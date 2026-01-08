817363e3c8c5f575be958dbb14bfe66abe8ef00a
Yaroslaw00 <yarikost901@gmail.com>
2026-01-08 18:35:29 +0100

    while for

---
+++ while.js
@@ -0,0 +1,34 @@
+// 1.
+// let i = 0;
+// while (i < 10) {
+//   i++;
+//   console.log(i);
+// }
+
+// 2.
+// for (let number = 2; number <= 20; number++) {
+//   if (number % 2 != 0) continue;
+//   console.log(number);
+// }
+
+// 3.
+// for (let number = 7; number <= 70; number++) {
+//   if (number % 7 != 0) continue;
+//   console.log(number);
+// }
+
+// 4.
+// let n = 0;
+// for (let n = 0; n <= 25; n++) {
+//   if (n >= 25) break;
+//   console.log(n);
+// }
+
+// 5
+
+// let n = 0;
+// while (n <= 20) {
+//   n++;
+//   if (n % 3 == 0) continue;
+//   console.log(n);
+// }
