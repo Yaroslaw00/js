64b3e37b194a18ae4d45e7762ac9616ee7c6eb26
Yaroslaw00 <yarikost901@gmail.com>
2026-01-15 19:45:00 +0100

    function

---
+++ function.js
@@ -0,0 +1,10 @@
+// const message = "Пусть каждый день приносит радость снова";
+// function changeString(message) {
+//   if (message.length > 30) {
+//     console.log(message.slice(0, 30) + "...");
+//   } else {
+//     console.log(message);
+//   }
+// }
+// changeString(message);
+// const message1 = "Пусть каждый день";
