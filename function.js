3882f79cee67b27715771cf13198a9c451573c42
Yaroslaw00 <yarikost901@gmail.com>
2026-01-22 20:44:41 +0100

    function

--- function.js
+++ function.js
@@ -8,3 +8,30 @@
 // }
 // changeString(message);
 // const message1 = "Пусть каждый день";
+
+// Напиши скрипт підрахунку вартості гравіювання прикрас. Для цього створи функцію calculateEngravingPrice(message, pricePerWord) приймаючу рядок (в рядку будуть тільки слова і прогалини) і ціну гравіювання одного слова, і повертає ціну гравіювання всіх слів в рядку.
+
+function calculateEngravingPrice(message, pricePerWord) {
+  const tomassiv = message.split(" ");
+
+  const wordnumber = tomassiv.length;
+  return pricePerWord * wordnumber;
+}
+const TotalPrice = calculateEngravingPrice("Хочу піти додому дуже сильно", 15);
+console.log(TotalPrice);
+
+// Напиши функцію findLongestWord(string), яка приймає параметром довільний рядок (в рядку будуть тільки слова і прогалини) і повертає найдовше слово в цьому рядку.
+
+function findLongestWord(string) {
+  const stringmassiv = string.split(" ");
+  let longword = stringmassiv[0];
+  for (let i = 0; i < stringmassiv.length; i++) {
+    const word = stringmassiv[i];
+    if (longword.length < word.length) {
+      longword = word;
+    }
+  }
+  return longword;
+}
+const longestword = findLongestWord("Я не можу грати в футбол");
+console.log(longestword);
